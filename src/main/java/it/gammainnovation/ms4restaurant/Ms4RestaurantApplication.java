5d918e1230e6a02c4e9c2896bd585b6007c31c19
frabarbara <francesco.barbara5697@gmail.com>
2023-11-07 16:56:29 +0100

    init commit

---
+++ src/main/java/it/gammainnovation/ms4restaurant/Ms4RestaurantApplication.java
@@ -0,0 +1,13 @@
+package it.gammainnovation.ms4restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ms4RestaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ms4RestaurantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/gammainnovation/ms4restaurant/Ms4RestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gammainnovation.ms4restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ms4RestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
